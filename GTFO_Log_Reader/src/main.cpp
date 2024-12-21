93df973ec6e2cf6ac5652867e00eef5d2a22da42
Nothing031 <160526040+Nothing031@users.noreply.github.com>
2024-12-21 18:32:46 +0900

    update

--- GTFO_Log_Reader/src/main.cpp
+++ GTFO_Log_Reader/src/main.cpp
@@ -8,12 +8,17 @@
 #include <mutex>
 
 #include <Windows.h>
+#include <conio.h>
+#include <mmsystem.h>
 
 #include <opencv2/opencv.hpp>
 #include <nlohmann/json.hpp>
 
 #include "__data.hpp"
 #include "LogReader.hpp"
+#include "resource.h"
+
+#pragma comment(lib, "winmm.lib")
 
 using namespace std;
 using namespace cv;
@@ -21,17 +26,20 @@ using namespace nlohmann;
 
 namespace fs = filesystem;
 
+// GLOBAL VARIABLE
 mutex printMutex;
 HWND hwnd;
 HWND hGTFO;
-HANDLE outputHandle;
-HANDLE inputHandle;
+HANDLE hConsoleOutput;
+HANDLE hConsoleInput;
 
 bool FORCEINJECT;
+//////////////////
+
 
 void gotoxy(int x, int y) {
 	COORD xy = { x, y };
-	SetConsoleCursorPosition(outputHandle, xy);
+	SetConsoleCursorPosition(hConsoleOutput, xy);
 }
 void PrintTyping(const string& _str, const bool& _endl, const int& delay) {
 	printMutex.lock();
@@ -42,7 +50,19 @@ void PrintTyping(const string& _str, const bool& _endl, const int& delay) {
 	if (_endl) cout << endl;
 	printMutex.unlock();
 }
-void PrintLoading() {
+void PrintTyping(const string& _str, const bool& _endl, const int& delay, const bool& reverse, const int& x, const int& y) {
+	printMutex.lock();
+	for (int i = _str.size(); i >= 0; i--) {
+		gotoxy(x+i, y);
+		cout << _str[i];
+		this_thread::sleep_for(chrono::milliseconds(delay));
+	}
+	if (_endl) cout << endl;
+	printMutex.unlock();
+}
+
+
+void PrintLoading(short x, short y) {
 	const int maxValue = 100;
 	const double initialDelay = 8;
 	const double factor = 1.12;
@@ -50,8 +70,9 @@ void PrintLoading() {
 	double delay = initialDelay;
 
 	for (int i = 0; i <= maxValue; ++i) {
-		std::cout << "\r" << i << "%";
-		std::this_thread::sleep_for(std::chrono::milliseconds(static_cast<int>(delay)));
+		gotoxy(x, y);
+		cout << i << "%";
+		this_thread::sleep_for(chrono::milliseconds(static_cast<int>(delay)));
 		if (i >= 70)
 			delay *= factor; // 딜레이를 점점 증가
 	}
@@ -105,6 +126,88 @@ void DisplayKeys(vector<key_t>& keys) {
 	cout << endl;
 
 }
+void __IntroText() {
+	gotoxy(22, 2);
+	SetConsoleTextAttribute(hConsoleOutput, 15);
+	cout << "SUBJECT ";
+	SetConsoleTextAttribute(hConsoleOutput, 4);
+	cout << "NOT ";
+	SetConsoleTextAttribute(hConsoleOutput, 15);
+	cout << "READY FOR CORTEX INTERFACE INJECTION";
+}
+void __Sleep(int millisecond) {
+	this_thread::sleep_for(chrono::milliseconds(millisecond));
+}
+void DisplayIntro() {
+	if (hGTFO == NULL) {
+		__Sleep(800);
+		PlaySound(MAKEINTRESOURCE(IDR_WAVE13), GetModuleHandle(NULL), SND_RESOURCE | SND_ASYNC);
+		__Sleep(200);
+		__IntroText();
+		__Sleep(20);
+		cout << "\r                                                                                 ";
+		__Sleep(20);
+		__IntroText();
+		__Sleep(20);
+		cout << "\r                                                                                 ";
+		__Sleep(48);
+		__IntroText();
+		__Sleep(50);
+		cout << "\r                                                                                 ";
+		__Sleep(50);
+		__IntroText();
+		__Sleep(50);
+		cout << "\r                                                                                 ";
+		__Sleep(50);
+		__IntroText();
+
+
+		int _count = 0;
+		while (hGTFO == NULL) {
+			if (_kbhit()) {
+				if (_getch() == 0x20) {
+					FORCEINJECT = true;
+					break;
+				}
+			}
+			this_thread::sleep_for(chrono::milliseconds(10));
+			hGTFO = FindWindowA(Keywords::GTFO_CLASS, Keywords::GTFO_CAPTION);
+		}
+	}
+	if (!FORCEINJECT) {
+		PrintTyping("                                                ", false, 7, true, 22, 2);
+		this_thread::sleep_for(chrono::milliseconds(700));
+		gotoxy(22, 2);
+		PrintTyping("SUBJECT READY FOR CORTEX INTERFACE INJECTION", true, 15);
+		this_thread::sleep_for(chrono::milliseconds(700));
+		gotoxy(22, 3);
+		PrintTyping("INJECTING", true, 5);
+		PrintLoading(32, 3);
+		this_thread::sleep_for(chrono::milliseconds(500));
+		system("cls");
+	}
+	else {
+		gotoxy(22, 3);
+		SetConsoleTextAttribute(hConsoleOutput, 4);
+		PrintTyping("OVERRIDE : ", false, 15);
+		this_thread::sleep_for(chrono::milliseconds(500));
+		PrintTyping("   ", true, 500, true, 30, 2);
+		cout << "   ";
+		this_thread::sleep_for(chrono::milliseconds(500));
+		gotoxy(33, 3);
+		PrintTyping("FORCE INJECT", true, 60);
+		this_thread::sleep_for(chrono::milliseconds(700));
+		SetConsoleTextAttribute(hConsoleOutput, 15);
+		gotoxy(22, 4);
+		PrintTyping("INJECTING ", false, 5);
+		PrintLoading(32, 4);
+		this_thread::sleep_for(chrono::milliseconds(500));
+		system("cls");
+	}
+
+}
+
+
 
 void Init() {
 	// init variable
@@ -112,8 +215,9 @@ void Init() {
 
 	// get handles
 	hwnd = GetConsoleWindow();
-	outputHandle = GetStdHandle(STD_OUTPUT_HANDLE);
-	inputHandle = GetStdHandle(STD_INPUT_HANDLE);
+	hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+	hConsoleInput = GetStdHandle(STD_INPUT_HANDLE);
+	hGTFO = FindWindowA(Keywords::GTFO_CLASS, Keywords::GTFO_CAPTION);
 
 	// set window
 	RECT rect;
@@ -121,35 +225,41 @@ void Init() {
 	MoveWindow(hwnd, rect.left, rect.top, 800, 400, false);
 
 	// hide cursor
-	CONSOLE_CURSOR_INFO cursorInfo;
-	GetConsoleCursorInfo(outputHandle, &cursorInfo);
-	cursorInfo.bVisible = FALSE;
-	SetConsoleCursorInfo(outputHandle, &cursorInfo);
+	CONSOLE_CURSOR_INFO cci;
+	GetConsoleCursorInfo(hConsoleOutput, &cci);
+	cci.bVisible = FALSE;
+	SetConsoleCursorInfo(hConsoleOutput, &cci);
 
 	// disable quick edit mode
 	DWORD mode;
-	if (GetConsoleMode(inputHandle, &mode)) {
+	if (GetConsoleMode(hConsoleInput, &mode)) {
 		mode &= ~ENABLE_QUICK_EDIT_MODE;
 		mode |= ENABLE_EXTENDED_FLAGS;
-		SetConsoleMode(inputHandle, mode);
+		SetConsoleMode(hConsoleInput, mode);
 	}
 
-	// set font
-	CONSOLE_FONT_INFOEX fontInfo;
-	fontInfo.cbSize = sizeof(CONSOLE_FONT_INFOEX);
-	GetCurrentConsoleFontEx(outputHandle, FALSE, &fontInfo);
-	fontInfo.dwFontSize.Y = 15;
-	SetCurrentConsoleFontEx(outputHandle, FALSE, &fontInfo);
-	
+	// hide scroll
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	GetConsoleScreenBufferInfo(hConsoleOutput, &csbi);
+	SMALL_RECT windowSize = csbi.srWindow;
+	COORD bufferSize = {
+		static_cast<SHORT>(windowSize.Right - windowSize.Left + 1),
+		static_cast<SHORT>(windowSize.Bottom - windowSize.Top + 1)
+	};
+	SetConsoleScreenBufferSize(hConsoleOutput, bufferSize);
 
+	// set clock resolution
+	timeBeginPeriod(1);
 }
+
+
 
 int main() {
 	// Init
 	Init();
 
 
-
+	//
 	// intergrity check
 	if (!fs::exists(".\\MAP"))
 		fs::create_directory(".\\MAP");
@@ -166,37 +276,7 @@ int main() {
 	LogReader logReader;
 
 	// intro
-	hGTFO = FindWindowA(Keywords::GTFO_CLASS, Keywords::GTFO_CAPTION);
-	if (hGTFO == NULL) {
-		PrintTyping("SUBJECT NOT READY FOR CORTEX INTERFACE INJECTION", false, 15);
-		int _count = 0;
-		while (hGTFO == NULL) {
-			if (GetAsyncKeyState(0x20) & 0x8000) {
-				FORCEINJECT = true;
-				break;
-			}
-			this_thread::sleep_for(chrono::milliseconds(10));
-			hGTFO = FindWindowA(Keywords::GTFO_CLASS, Keywords::GTFO_CAPTION);
-		}
-	}
-	if (!FORCEINJECT) {
-		cout << "\r                                                                                        \r";
-		PrintTyping("SUBJECT READY FOR CORTEX INTERFACE INJECTION", true, 15);
-		this_thread::sleep_for(chrono::milliseconds(500));
-		PrintTyping("INJECTING", true, 5);
-		PrintLoading();
-		this_thread::sleep_for(chrono::milliseconds(500));
-	}
-	else {
-		cout << endl;
-		PrintTyping("OVERRIDE : ", false, 15);
-		this_thread::sleep_for(chrono::milliseconds(500));
-		PrintTyping("FORCE INJECT", true, 60);
-		this_thread::sleep_for(chrono::milliseconds(500));
-		PrintTyping("INJECTING", true, 5);
-		PrintLoading();
-		this_thread::sleep_for(chrono::milliseconds(500));
-	}
+	DisplayIntro();
 
 	
 
